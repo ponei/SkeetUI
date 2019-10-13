44502a212acdcb5dfb7d8f7d2f278c8a0db7f2cf
ponei <poneialt@gmail.com>
2019-10-13 11:03:11 -0300

    new controls
    started developing those :eyes:

---
+++ SkeetUI/skeetColorPicker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SkeetUI
+{
+    public partial class skeetColorPicker : Panel
+    {
+        public skeetColorPicker()
+        {
+            InitializeComponent();
+        }
+    }
+}
